8eb50d0981810262d4430f3ed08428c7ad1868e0
janset602 <janset602@myemail.northland.edu>
2017-06-21 10:04:35 -0500

    Macro's, function wrappers, and bug fix
    If proc could not find last_w, time_val and continue, never reaching file reset

--- A_Proc.c
+++ A_Proc.c
@@ -1,13 +1,13 @@
 /*
- * A_Proc.c
- *
- *  Created on: Jun 19, 2017
- *      Author: thomasjansen
- */
+* A_Proc.c
+*
+*  Created on: Jun 19, 2017
+*      Author: thomasjansen
+*/
 
 
-#include "arghelp/arghelp.h"
-#include "strl/strl.h"
+#include "arghelp.h"
+#include "strl.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -17,42 +17,53 @@
 #include <unistd.h>
 #endif
 
-#ifdef OS_WINDOWS
+#ifdef _WIN32
 #include <Windows.h>
+#include <direct.h>
 #endif
 
 #include <time.h>
-#include "word/word.h"
+#include "word.h"
 
 #ifdef __APPLE__
 int MAX_PATH;
 #endif
 
-int mkdir_t( char* filename ) {
+int mkdir_t(char* filename) {
 
 #ifdef __APPLE__
-	return mkdir( filename , S_IRWXU );
+	return mkdir(filename, S_IRWXU);
 #endif
 
-#ifdef OS_WINDOWS
-	return _mkdir( filename );
+#ifdef _WIN32
+	return _mkdir(filename);
 #endif
 
 }
 
-int search_mw( FILE* fd , char* buff ) {
-	return search_w( fd , buff , strlength( buff , '\0' ));
+void sleep_t(int t_secs) {
+#ifdef __APPLE__
+	sleep(t_secs);
+#endif
+
+#ifdef _WIN32
+	Sleep(t_secs * 100);
+#endif
 }
 
-word* create_mword( char* buff ) {
-	return create_word( buff , strlength( buff , '\0' ));
+int search_mw(FILE* fd, char* buff) {
+	return search_w(fd, buff, strlength(buff, '\0'));
 }
 
-int word_mcmp( word* w , char* buff ) {
+word* create_mword(char* buff) {
+	return create_word(buff, strlength(buff, '\0'));
+}
+
+int word_mcmp(word* w, char* buff) {
 	int ans;
-	word* w1 = create_mword( buff );
-	ans = word_cmp( w , w1 );
-	destroy_word( w1 );
+	word* w1 = create_mword(buff);
+	ans = word_cmp(w, w1);
+	destroy_word(w1);
 	return ans;
 }
 
@@ -71,162 +82,163 @@ void find_rep(word* w, char f, char r) {
 }
 
 struct tm get_time() {
-	time_t t = time( NULL );
-	return *localtime( &t );
+	time_t t = time(NULL);
+	return *localtime(&t);
 }
 
-int write_err( char* err_c , char* buff ) {
-	FILE* errfd = fopen( err_c , "a" );
-	if ( errfd == ( FILE* )NULL )
+int write_err(char* err_c, char* buff) {
+	FILE* errfd = fopen(err_c, "a");
+	if (errfd == (FILE*)NULL)
 		return -1;
-	fwrite( buff , strlength( buff , '\0' ) , 1 , errfd );
-	fclose( errfd );
+	fwrite(buff, strlength(buff, '\0'), 1, errfd);
+	fclose(errfd);
 	return 0;
 }
 
-int main( int argc , char** argv ) {
-	char* src_d = getstring( argc , argv , 's' );
-	char* dest_d = getstring( argc , argv , 'd' ); //May not exist
+int main(int argc, char** argv) {
+	char* src_d = getstring(argc, argv, 's');
+	char* dest_d = getstring(argc, argv, 'd'); //May not exist
 
-	int time_val = getint( argc , argv , 't' );
-	if ( time_val == -1 )
+	int time_val = getint(argc, argv, 't');
+	if (time_val == -1)
 		time_val = 0;
 
-	if ( mkdir_t( dest_d ) < 0 && errno != EEXIST ) {
-		printf( "Failed to mkdir\n" );
+	if (mkdir_t(dest_d) < 0 && errno != EEXIST) {
+		printf("Failed to mkdir\n");
 		return -1;
 	}
 #ifdef __APPLE__
-	if (( MAX_PATH = pathconf( dest_d , _PC_NAME_MAX )) < 0 ) { //Gets Max Path Name for dest_d
-		printf( "Bad MP\n" );
+	if ((MAX_PATH = pathconf(dest_d, _PC_NAME_MAX)) < 0) { //Gets Max Path Name for dest_d
+		printf("Bad MP\n");
 		return MAX_PATH;
 	}
 #endif
 
 	struct tm ttime;
-	FILE *rfd , *wfd;
-	char* err_c = getstring( argc , argv , 'e' );
-	int flags , count_d;
-	char* buff = ( char* )malloc( MAX_PATH + 1 );
+	FILE *rfd, *wfd;
+	char* err_c = getstring(argc, argv, 'e');
+	int flags, count_d;
+	char* buff = (char*)malloc(MAX_PATH + 1);
 
-	word* last_w = create_mword( "[measurements]" );
+	word* last_w = create_mword("[measurements]");
 	word* f_name;
 
-	while ( 1 ) {
+	while (1) {
 		ttime = get_time();
-		flags = snprintf( buff , MAX_PATH , "%s/%d-0%d-%d.log" , src_d , ttime.tm_year + 1900 , ttime.tm_mon + 1 , ttime.tm_mday );
-		if (( rfd = fopen( buff , "r" )) == ( FILE* )NULL ) {
-			sleep( time_val );
+		flags = snprintf(buff, MAX_PATH, "%s/%d-0%d-%d.log", src_d, ttime.tm_year + 1900, ttime.tm_mon + 1, ttime.tm_mday);
+		if ((rfd = fopen(buff, "r")) == (FILE*)NULL) {
+			sleep_t(time_val);
 			continue;
 		}
 
-		flags = snprintf( buff , MAX_PATH , "%s/%d.%d.%d" , dest_d , ttime.tm_year + 1900 , ttime.tm_mon + 1 , ttime. tm_mday );
-		if ( mkdir_t( buff ) < 0 && errno != EEXIST ) {
+		flags = snprintf(buff, MAX_PATH, "%s/%d.%d.%d", dest_d, ttime.tm_year + 1900, ttime.tm_mon + 1, ttime.tm_mday);
+		if (mkdir_t(buff) < 0 && errno != EEXIST) {
 			/*
-			 * Other issues
-			 */
-			write_err( err_c , "Failed to make: " );
-			write_err( err_c , buff );
-			write_err( err_c , "\n" );
+			* Other issues
+			*/
+			write_err(err_c, "Failed to make: ");
+			write_err(err_c, buff);
+			write_err(err_c, "\n");
 			return -1;
 		}
 
-		flags = search_mw( rfd , "Name:" );
-		if ( flags == EOF ) {
-			fclose( rfd );
-			sleep( time_val );
+		flags = search_mw(rfd, "Name:");
+		if (flags == EOF) {
+			fclose(rfd);
+			sleep_t(time_val);
 			continue;
 		}
 
-		f_name = read_t( rfd , &flags , 1024 );
-		if ( flags == EOF ) {
-			fclose( rfd );
-			if ( f_name != ( word* )NULL )
-				destroy_word( f_name );
-			sleep( time_val );
+		f_name = read_t(rfd, &flags, 1024);
+		if (flags == EOF) {
+			fclose(rfd);
+			if (f_name != (word*)NULL)
+				destroy_word(f_name);
+			sleep_t(time_val);
 			continue;
 		}
 
-		flags = snprintf( buff , MAX_PATH , "%s/%d.%d.%d/%s.txt" , dest_d , ttime.tm_year + 1900 , ttime.tm_mon + 1 , ttime.tm_mday , f_name->buff );
-		if (( wfd = fopen( buff , "w" )) == ( FILE* )NULL ) { //Switch back to a later on
-			/*
-			 * bigger issues
-			 */
-			fclose( rfd );
-			printf( "Failed to create/open wfd: %s\n" , buff );
-			write_err( err_c , "Failed to create/open wfd: " );
-			write_err( err_c , buff );
-			write_err( err_c , "\n" );
+		flags = snprintf(buff, MAX_PATH, "%s/%d.%d.%d/%s.txt", dest_d, ttime.tm_year + 1900, ttime.tm_mon + 1, ttime.tm_mday, f_name->buff);
+		if ((wfd = fopen(buff, "a")) == (FILE*)NULL) { //Switch back to a later on
+													   /*
+													   * bigger issues
+													   */
+			fclose(rfd);
+			printf("Failed to create/open wfd: %s\n", buff);
+			write_err(err_c, "Failed to create/open wfd: ");
+			write_err(err_c, buff);
+			write_err(err_c, "\n");
 			return -1;
 		}
-		destroy_word( f_name );
+		destroy_word(f_name);
 		/*
-		 * Setup Complete
-		 * Raw data has been located
-		 * Destination has been created
-		 */
+		* Setup Complete
+		* Raw data has been located
+		* Destination has been created
+		*/
 
-		if ( word_mcmp( last_w , "[measurements]" )) {
-			if (( flags = search_w( rfd , last_w->buff , last_w->len )) == EOF ) {
-				fclose( rfd );
-				fclose( wfd );
-				sleep( time_val );
+		if (word_mcmp(last_w, "[measurements]")) {
+			if ((flags = search_w(rfd, last_w->buff, last_w->len)) == EOF) {
+				fclose(rfd);
+				fclose(wfd);
+				sleep_t(time_val);
 				continue;
 			}
-			word* header = read_t( rfd , &flags , 1024 );
-			if ( flags == EOF ) {
-				fclose( rfd );
-				fclose( wfd );
-				if ( header != ( word* )NULL )
-					destroy_word( header );
-				sleep( time_val );
+			word* header = read_t(rfd, &flags, 1024);
+			if (flags == EOF) {
+				fclose(rfd);
+				fclose(wfd);
+				if (header != (word*)NULL)
+					destroy_word(header);
+				sleep_t(time_val);
 				continue;
 			}
 
-			count_d = count( header , ';' );
-			find_rep( header , ';' , ',' );
-			fwrite( header->buff , header->len , 1 , wfd );
-			fputc( '\n' , wfd );
-			destroy_word( header );
-			destroy_word( last_w );
-			last_w = create_mword( "[start]" );
+			count_d = count(header, ';');
+			find_rep(header, ';', ',');
+			fwrite(header->buff, header->len, 1, wfd);
+			fputc('\n', wfd);
+			destroy_word(header);
+			destroy_word(last_w);
+			last_w = create_mword("[start]");
 		}
 
-		if ( search_w( rfd , last_w->buff , last_w->len ) == EOF ) {
-			fclose( rfd );
-			fclose( wfd );
-			sleep( time_val );
+		if (search_w(rfd, last_w->buff, last_w->len) == EOF) {
+			fclose(rfd);
+			fclose(wfd);
+			if (ttime.tm_hour == 1) {
+				//Delete rfd
+				printf("New File\n");
+				destroy_word(last_w);
+				last_w = create_mword("[measurements]");
+			}
+			sleep_t(time_val);
 			continue;
 		}
 
 		flags = 0;
 		word* w;
-		while( flags != EOF ) {
-			if (( w = read_t( rfd , &flags , 1024 )) == ( word* )NULL )
+		while (flags != EOF) {
+			if ((w = read_t(rfd, &flags, 1024)) == (word*)NULL)
 				continue;
-			if ( count_d != count( w , ';' )) { //incomplete data point
-				destroy_word( w );
+			if (count_d != count(w, ';')) { //incomplete data point
+				destroy_word(w);
 				continue;
 			}
 
-			destroy_word( last_w );
-			last_w = create_word( w->buff , w->len );
-			find_rep( w , ';' , ',' );
-			fwrite( w->buff , w->len , 1 , wfd );
-			fputc( '\n' , wfd );
-			destroy_word( w );
+			destroy_word(last_w);
+			last_w = create_word(w->buff, w->len);
+			find_rep(w, ';', ',');
+			fwrite(w->buff, w->len, 1, wfd);
+			fputc('\n', wfd);
+			destroy_word(w);
 		}
 
-		fclose( rfd );
-		fclose( wfd );
+		fclose(rfd);
+		fclose(wfd);
 
-		if ( ttime.tm_hour == 1 ) {
-			//Delete rfd
-			destroy_word( last_w );
-			last_w = create_mword( "[measurements]" );
-		}
-		printf( "End\n" );
-		sleep( time_val );
+		printf("End\n");
+		sleep_t(time_val);
 	}
 
 	return 0;
